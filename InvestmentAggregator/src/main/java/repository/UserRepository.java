09b009020814adcba995244bcf1c7865f8c8236f
limaismael8901 <limaismael8901@gmail.com>
2024-11-15 07:11:07 -0300

    :lock::sparkles:feat(security): adiciona segurança com JWT, Spring Security e OAuth2
    - Implementado AuthenticationService para autenticação de usuários.
    - Criado JwtService para geração de tokens JWT com claims e escopos
    baseados nas autoridades do usuário.
    - Adicionado UserAuthenticated para representar detalhes de autenticação
    do usuário.
    - Implementado UserDetailsService customizado (UserDatailsServiceImpl)
    para carregamento de usuários a partir do repositório.
    - Configurada a cadeia de segurança (SecurityFilterChain) com suporte a
    autenticação JWT e desabilitação de CSRF.
    - Adicionada codificação de senha com BCryptPasswordEncoder.
    - Configurado JwtEncoder e JwtDecoder utilizando chaves RSA para
    codificação/decodificação dos tokens JWT.

--- InvestmentAggregator/src/main/java/repository/UserRepository.java
+++ InvestmentAggregator/src/main/java/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID>{
+	
+	Optional<User> findByUsername(String username);
 	
 }
---
+++ InvestmentAggregator/src/main/java/security/UserDatailsServiceImpl.java
@@ -0,0 +1,25 @@
+package security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import repository.UserRepository;
+
+public class UserDatailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+	
+	public UserDatailsServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return userRepository.findByUsername(username)
+				.map(user -> new UserAuthenticated(user))
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+	}
+}
